c02ecf1393521aea9cbe3d8a59abafc4762e38da
Nathan <nathansaccon10@hotmail.com>
2020-09-15 11:33:41 -0400

    Class 2 updates

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 import "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.js";
 import "https://cdnjs.cloudflare.com/ajax/libs/marked/1.1.1/marked.js";
 import aPages from "../pages/index.js";
+import "https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js";
+
+const sName = "Nate Saccon";
 
 class Page {
     render(){
@@ -36,10 +39,41 @@ class Article extends Page{
 
 class Footer extends Page{
     render(){
-        const sName = "Nate Saccon";
         const yToday = new Date().getFullYear();
         $("footer").html(
             `&copy; ${yToday} ${sName}`
+        );
+    }
+}
+
+class Nav extends Page{
+    render(){
+        let sMenu = "";
+        for(let n=0; n < aPages.length; n++){
+            const sMenuItem = aPages[n].title;
+            if(sMenuItem != "index"){
+                sMenu += `<li><a href="#${sMenuItem}">${sMenuItem}</a></li>`
+            }
+        }
+
+        $("nav").html(
+            `<div class="navbar navbar-inverse navbar-static-top" role="navigation">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#home">Portfolio of ${sName}</a>
+            </div>
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav navbar-right">
+                   ${sMenu}
+                </ul>
+            </div>
+    
+        </div>`
         );
     }
 }
@@ -48,7 +82,7 @@ class Portfolio extends Page{
     constructor(){
         super();
         this.header = new Page();
-        this.nav = new Page();
+        this.nav = new Nav();
         this.article = new Article();
         this.footer = new Footer();
     }
